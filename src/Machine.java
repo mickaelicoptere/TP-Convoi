b93d30db862ee3e52810457065f5f5dffd5371f1
Mickael Lanier <mickael.lanier@etu.unice.fr>
2017-12-12 15:23:26 +0100

    :hankey:

--- src/Machine.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by shuwn on 05/12/2017.
- */
-public interface Machine {
-    int getWeight();
-    int getHorsePower();
-}
--- src/Simulator.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by shuwn on 05/12/2017.
- */
-public class Simulator {
-    public static double consumption (Machine machine, int speed) {
-        return speed* (machine.getHorsePower() + machine.getWeight());
-    }
-}
